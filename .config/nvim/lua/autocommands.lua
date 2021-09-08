099c5c2418a175d676851d35bed0510e593719f3
Fred White <fred-white@hotmail.co.uk>
2021-09-08 14:23:42 +0100

    add some more autocommands

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -8,3 +8,38 @@ utils.define_augroups({
         {'BufNewFile', '*', 'lua vim.opt.formatoptions:remove({\'c\', \'o\', \'r\'})'},
     }
 })
+
+-- Highlight on yank
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]],
+  false
+)
+
+-- Change tabs to be 4 spaces for python files only
+vim.api.nvim_exec(
+  [[
+    autocmd FileType python setl shiftwidth=4 sts=4 ts=2 et
+  ]],
+  false
+)
+
+-- Auto-resize splits when Vim gets resized.
+vim.api.nvim_exec(
+  [[
+    autocmd VimResized * wincmd =
+  ]],
+  false
+)
+
+-- Update a buffer's contents on focus if it changed outside of Vim.
+vim.api.nvim_exec(
+  [[
+    au FocusGained,BufEnter * :checktime
+  ]],
+  false
+)
